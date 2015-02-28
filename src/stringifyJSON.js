40a455453566c0ce710412d676859419a871f37a
Bill Kilmer <wdk122@gmail.com>
2015-02-28 12:09:46 -0800

    Implements stringifyJSON for arrays.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,47 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+
+var stringifyJSON = function(input) {
+  var result = '';
+  // handle non-objects
+  
+  if (input === null) {
+    result += 'null';
+    return result;
+  }
+
+  if (typeof input !== 'object') {
+    if (typeof input === 'string') {
+      result += '"' + input + '"';
+    } else {
+      result += input;
+    }
+    return result;
+  }
+  
+  if (Array.isArray(input)) {
+    var arrayResult = "[";
+
+    for (var i = 0; i < input.length; i++) {
+      arrayResult += stringifyJSON(input[i]);
+      if (i !== input.length - 1) {
+        arrayResult += ",";
+      }
+    }
+    
+    result += arrayResult + "]";
+    // use recursion here
+  }
+
+
+  return result;
 };
+
+// typeof null returns object
+
+
+
+/*
+if input is array, 
+*/
